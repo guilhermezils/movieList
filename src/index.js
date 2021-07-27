1b7fc78e6bf4dd5a9c02edcb24897dfe03829a79
guilhermezils <guilhermezils@icloud.com>
2021-07-26 21:51:12 -0500

    trial

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react';
 
 const App = () => {
-    return <h1>movieList</h1>
+    return <h1>movies List</h1>
 };
 
 ReactDOM.render(<App /> , document.getElementById('root'));
